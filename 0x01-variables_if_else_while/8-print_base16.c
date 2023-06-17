b666a1c6455b8372b2ec54574960135ce4d01ad8
Ifeoluwa2001 <ajiboyeifeoluwa100@gmail.com>
2023-06-16 21:29:54 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f.
+ * main - Prints the range of number from 0 - 9 and letters a - f.
  *
  * Return: Always 0 (Success)
  */
