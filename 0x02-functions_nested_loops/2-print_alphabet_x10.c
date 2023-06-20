5a2dd52d571b3e9104d5752dda3c7ca72ec21d56
Ifeoluwa2001 <ajiboyeifeoluwa100@gmail.com>
2023-06-20 14:51:44 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,15 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-	
-	
-	i = 0;
 
+	i = 0;
 
 	while (i < 10)
 	{
 		ch = 'a';
-		while(ch <= 'z')
+		while (ch <= 'z')
 	{
-		_putchar(ch);
+		_putchar (ch);
 		ch++;
 	}
 	_putchar('\n');
