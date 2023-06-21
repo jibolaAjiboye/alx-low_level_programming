b935d5d6715e0f803c6384121d0386c04810fb97
Ifeoluwa2001 <ajiboyeifeoluwa100@gmail.com>
2023-06-21 14:39:55 -0700

    finally

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,8 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
- */
+ * return: empty output
+*/
 
 void print_times_table(int n)
 {
@@ -16,26 +17,33 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
-
 				if (j == 0)
+				{
 					_putchar(k + '0');
-				else
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-
-					if (k < 10)
-						-putchar(' ');
-					if (k < 100)
-						_putchar(' ');
-
-					if (k >= 100)
-						_putchar((k / 100) + '0');
-					if (k >= 10)
-						_putchar(((k / 10) % 10) + '0');
-					-putchar((k % 10) + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
+			_putchar('\n');
 		}
 	}
 }
