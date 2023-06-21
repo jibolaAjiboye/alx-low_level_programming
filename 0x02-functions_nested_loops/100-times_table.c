32b4ce52280d3314cacededd34bac3790fc7510c
Ifeoluwa2001 <ajiboyeifeoluwa100@gmail.com>
2023-06-21 05:21:43 -0700

    Task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* print_times_table - prints the n times table
+* starting with 0
+* @n: number of the times table
+*/
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int k = j * i;
+
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else
+				{
+					_putchar(',');
+					if (k < 10)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+					else if (k < 100)
+					{
+						_putchar(',');
+					}
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+
